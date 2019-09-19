539a37d9c75fb79b99c70779a7ea5a265461d2d6
Doug Kintop <dkintop@hotmail.com>
2019-09-19 19:18:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,18 +6,9 @@ class Anagram
     
     @word = word
     
-    
-    def anagram(word_array)
-      #use .select
-      binding.pry
-      word_array.select do |the_word|
-         
-        the_word.split("").sort == @word.split("").sort
-       
-      end 
-    end
-    
   end 
+  
+  
   
   
 end 
